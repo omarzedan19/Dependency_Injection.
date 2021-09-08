61baa414ce44598db410751f73fe98f65fe31ba1
omarzedan19 <67125729+omarzedan19@users.noreply.github.com>
2021-09-08 12:20:43 +0300

    Add files via upload

---
+++ test_project/test_project/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using test_project.Controllers.models;
+
+namespace test_project.Controllers
+{
+    [Controller]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        Iuser Myusers = null;
+
+        public UserController(Iuser firstuser)
+        {
+            Myusers = firstuser;
+        }
+
+
+        [HttpPost]
+        public void Adduser( [FromBody] User user)
+        {
+            Myusers.Adduser(user);
+        }
+
+        [HttpDelete("{id}")]
+        public void Deleteuser(int id)
+        {
+            Myusers.Deleteuser(id);
+
+        }
+
+        [HttpGet]
+        public List<User> GetAllUsers()
+        {
+          return  Myusers.GetAllUsers();
+        }
+
+        [HttpGet("{id}")]
+        public User GetUser(int id)
+        {
+            return Myusers.GetUser(id);
+        }
+    }
+
+}
---
+++ test_project/test_project/Controllers/models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test_project.Controllers.models
+{
+    public class User
+    {
+        public User(string name, DateTime dob, string phone, string email, int id)
+        {
+            Name = name;
+            Dob = dob;
+            Phone = phone;
+            Email = email;
+            this.id = id;
+        }
+
+        public User()
+        {
+           
+        }
+
+        public string Name { get; set; }
+        public DateTime Dob { get; set; }
+
+        public string Phone { get; set; }
+
+        public string  Email { get; set; }
+
+        public int id { get; set; }
+
+
+    }
+}
